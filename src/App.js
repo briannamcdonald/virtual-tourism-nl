24f2ec2e177fc79042ad94c205af20ccd660c943
Dilum1999 <dilum14@gmail.com>
2021-02-19 17:47:14 -0800

    added the project structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import { Text } from "@chakra-ui/react"
+import Home from './components/home/home'
+import Navbar from './components/navbar/navbar'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Text>Test Text</Text>
+      <Navbar/>
+    <Router>
+      <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
     </div>
   );
 }
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,4 @@
+function Navbar() {
+    return <h2>Home</h2>;
+  }
+export default Navbar
