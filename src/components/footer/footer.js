7e44ac2adbb5a31596efa72bb615b544b8aeac1d
Dilum1999 <dilum14@gmail.com>
2021-02-20 17:46:09 -0800

    implemeted the footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,38 @@
+import { Box, Flex, Text,Link } from "@chakra-ui/react";
+import { FaInstagram, FaFacebook,FaPinterest, FaTwitter } from 'react-icons/fa';
+
 function Footer(){
     return(
-        <div>Footer</div>
+        <Box margin="20px">
+            <Flex justifyContent="space-evenly" height="150px">
+                <Flex p="10px" flexDirection="column">
+                    <Text fontWeight="bold" >Policies</Text>
+                    <Link>Terms of Service</Link>
+                    <Link>Privacy Policy</Link>
+                    <Link>Cancellation Policy</Link>
+                </Flex>
+                <Flex p="10px" flexDirection="column">
+                    <Text fontWeight="bold" >FAQs</Text>
+                    <Link>Users</Link>
+                    <Link>Providers</Link>
+                </Flex>
+                <Flex p="10px" flexDirection="column">
+                    <Text fontWeight="bold">Contact and Support</Text>
+                    <Link>Contact Us</Link>
+                    <Link>Media Enquiries</Link>
+                </Flex>
+            </Flex>
+            <hr/>
+            <Flex margin="10px" justifyContent="space-between">
+                <Text fontSize="0.8rem">&copy; 2021 NL Life. All Rights Reserved.</Text>
+                <Flex minW="150px" justifyContent="space-between">
+                    <FaFacebook />
+                    <FaInstagram/>
+                    <FaPinterest/>
+                    <FaTwitter/>
+                </Flex>
+            </Flex>
+        </Box>
     )
 }
 export default Footer;
